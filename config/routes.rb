a91c41ae611654fc074d4f9787d3d104583bb39a
Mohd Rizvan <mohd.rizvan@navyuginfo.com>
2018-05-05 21:24:20 +0530

    project resource created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :projects
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "welcome#index"
+  root to: "projects#index"
   get 'welcome/index'
 end
