f3bbcc921e31e6ee7fde03db4ed588a9b0b30372
Mohd Rizvan <mohd.rizvan@navyuginfo.com>
2018-05-13 11:42:26 +0530

    projects status table for admin

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'environments/environment';
+const API_URL = environment.apiURL;
 
 @Component({
   selector: 'app-admin',
@@ -6,8 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  allTodos;
+  projects;
+  constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient) {
+    this.getAllTodos();
+  }
 
-  constructor() { }
+  getAllTodos(){
+    this.http.get(API_URL +"/todos")
+      .subscribe(
+        (response) => {
+        this.allTodos = response;
+        this.getAllProjects(this.allTodos);
+      },
+        (error)=>{
+        console.log(error);
+    })
+  }
+
+  getAllProjects(todos){
+    this.projects = todos.map(todo => todo.project_name)
+  }
+
+  filterProjectTodos(project, status){
+    console.log("");
+    let doneTodos =  this.allTodos.filter(function (e) {
+      return e.status == status &&
+       e.project_name == project
+    });
+    return doneTodos;
+  }
+
+
 
   ngOnInit() {
   }
