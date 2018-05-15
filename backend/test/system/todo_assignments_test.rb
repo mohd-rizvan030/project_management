4ae0111e64239ffbfd71b4f165d9b163c37aec88
Mohd Rizvan <mohd.rizvan@navyuginfo.com>
2018-05-15 20:39:28 +0530

    test folder removed

--- backend/test/system/todo_assignments_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class TodoAssignmentsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit todo_assignments_url
-  #
-  #   assert_selector "h1", text: "TodoAssignment"
-  # end
-end
